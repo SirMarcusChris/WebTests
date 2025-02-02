a068b3a04370e6d0ff19c53affff85a8c2b3e484
admin <denis@1>
2025-02-02 14:16:12 +0700

    first lesson without other services of registration

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,9 +1,16 @@
 from pages.BasePage import BasePage
 from selenium.webdriver.common.by import By
 
+
 class LoginPageLocators:
-    LOGIN_FIELD = (By.XPATH, '//*[@id="field_email"]')  #  Если есть id, можно написать By.ID и искать по ID
-    LOGIN_BUTTON =(By.XPATH, '//*[@data-l="t,sign_in"]')
+    LOGIN_BY_QR_TAB = (By.XPATH, '//*[data-l="t,get_qr"]')
+    LOGIN_TAB = (By.XPATH, '//*[data-l="t,login_tab"]')
+    LOGIN_FIELD = (By.XPATH, '//*[@id="field_email"]')  # Если есть id, можно написать By.ID и искать по ID
+    PASSWORD_FIELD = (By.XPATH, '//*[@id="field_password"]')
+    LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,sign_in"]')
+    LOGIN_BY_QR_BUTTON = (By.XPATH, '//*[@id="qrCode-4532853097"]')
+    FORGOT_PASSWORD_BUTTON = (By.XPATH, '//*[data-l="t,restore"]')
+    REGISTRATION_BUTTON = (By.XPATH, '//*[@data-l="t,restore""]')
 
 
 class LoginPageHelper(BasePage):
