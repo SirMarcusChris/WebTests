aa3e6a4e354bf30e3dd5e143d9c7ce63e43092e6
admin <denis@1>
2025-02-02 22:07:39 +0700

    after find element writen

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -10,8 +10,26 @@ class LoginPageLocators:
     LOGIN_BUTTON = (By.XPATH, '//*[@data-l="t,sign_in"]')
     LOGIN_BY_QR_BUTTON = (By.XPATH, '//*[@id="qrCode-4532853097"]')
     FORGOT_PASSWORD_BUTTON = (By.XPATH, '//*[data-l="t,restore"]')
-    REGISTRATION_BUTTON = (By.XPATH, '//*[@data-l="t,restore""]')
+    REGISTRATION_BUTTON = (By.XPATH, '//*div[@class="external-oauth-login-footer"]/a[@data-l="t,register"]')
+    VK_BUTTON = (By.XPATH, '//*[@data-l="t,mailru"]')
+    MAIL_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
+    YANDEX_BUTTON = (By.XPATH, '//*[@data-l="t,yandex"]')
 
 
 class LoginPageHelper(BasePage):
-    pass
+    def __init__(self, driver):
+        self.driver = driver
+        self.check_page()
+
+    def check_page(self):
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_TAB)
+        self.find_element(LoginPageLocators.LOGIN_TAB)
+        self.find_element(LoginPageLocators.LOGIN_FIELD)
+        self.find_element(LoginPageLocators.PASSWORD_FIELD)
+        self.find_element(LoginPageLocators.LOGIN_BUTTON)
+        self.find_element(LoginPageLocators.LOGIN_BY_QR_BUTTON)
+        self.find_element(LoginPageLocators.FORGOT_PASSWORD_BUTTON)
+        self.find_element(LoginPageLocators.REGISTRATION_BUTTON)
+        self.find_element(LoginPageLocators.VK_BUTTON)
+        self.find_element(LoginPageLocators.MAIL_BUTTON)
+        self.find_element(LoginPageLocators.YANDEX_BUTTON)
